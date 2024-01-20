e82017aeb18872a9d79f57c8115f3145cbc1cf9c
Przemysław Wierzbicki <wierzbicki.przemek@gmail.com>
2024-01-20 09:35:07 +0100

    add Wizyta models

---
+++ Models/Wizyta.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Stomatologia.Models
+{
+    public class Wizyta
+    {
+            public int Id { get; set; }
+            public DateTime WybranaData { get; set; }
+            public string WybranaGodzina { get; set; }
+       
+        [Column("WybranyStomatologId")]
+        public string WybranyStomatologId { get; set; }
+        //public Stomatolog? Stomatolog { get; set; }
+    }
+    }
+
